2a6bcdd77d1b791b8518681113dabf391629a193
eminyuce <eminyuce@gmail.com>
2015-05-01 09:20:06 -0400

    Commit from VS

--- StoreManagement/StoreManagement.Service/Repositories/Interfaces/IStoreUserRepository.cs
+++ StoreManagement/StoreManagement.Service/Repositories/Interfaces/IStoreUserRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GenericRepository.EntityFramework;
+﻿using GenericRepository.EntityFramework;
 using StoreManagement.Data.Entities;
 
 namespace StoreManagement.Service.Repositories.Interfaces

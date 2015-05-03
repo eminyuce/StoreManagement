9204ac44b0f81e922ba38afae07e12a8e0e6b7e8
EMINYUCE <eminyuce@gmail.com>
2015-05-03 09:58:57 -0700

    add

---
+++ StoreManagement/StoreManagement.Admin/obj/Release/Package/PackageTmp/Scripts/FileUpload/main.js
@@ -0,0 +1,26 @@
+/*
+ * jQuery File Upload Plugin JS Example 6.5.1
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*jslint nomen: true, unparam: true, regexp: true */
+/*global $, window, document */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload();
+
+    $('#fileupload').fileupload('option', {
+            maxFileSize: 500000000,
+            resizeMaxWidth: 1920,
+            resizeMaxHeight: 1200
+        });
+});

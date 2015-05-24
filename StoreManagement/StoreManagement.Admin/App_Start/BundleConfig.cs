11f8b741a49fe4d61cad94df105ebf47d6a79fbd
EMINYUCE <eminyuce@gmail.com>
2015-05-24 14:58:22 -0700

    Site.css is changed

--- StoreManagement/StoreManagement.Admin/App_Start/BundleConfig.cs
+++ StoreManagement/StoreManagement.Admin/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace StoreManagement.Admin
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/core.css",

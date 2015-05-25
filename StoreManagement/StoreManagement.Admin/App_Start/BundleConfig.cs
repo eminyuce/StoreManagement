5422279710ab7052cc74ba37b8c45c0f48047069
EMINYUCE <eminyuce@gmail.com>
2015-05-25 11:20:36 -0700

    Chnage 1

--- StoreManagement/StoreManagement.Admin/App_Start/BundleConfig.cs
+++ StoreManagement/StoreManagement.Admin/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace StoreManagement.Admin
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 

0f185995a33c146f847de1e53926dfb7102768bb
eminyuce <eminyuce@gmail.com>
2015-05-26 09:58:44 -0400

    Fixing a bug in general helper

--- StoreManagement/StoreManagement.Data/GeneralHelper/GeneralHelper.cs
+++ StoreManagement/StoreManagement.Data/GeneralHelper/GeneralHelper.cs
@@ -876,5 +876,19 @@ namespace StoreManagement.Data.GeneralHelper
             }
             return new String(list.ToArray());
         }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
     }
 }

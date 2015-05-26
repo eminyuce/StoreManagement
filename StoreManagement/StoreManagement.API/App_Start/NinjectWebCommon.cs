0a7ef7d27b9b2c7b4bad5fa780c58b24f81b19ff
eminyuce <eminyuce@gmail.com>
2015-05-26 17:14:02 -0400

    Store Management API project first commit

---
+++ StoreManagement/StoreManagement.API/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,82 @@
+using StoreManagement.Service.DbContext;
+using StoreManagement.Service.Repositories;
+using StoreManagement.Service.Repositories.Interfaces;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(StoreManagement.API.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(StoreManagement.API.App_Start.NinjectWebCommon), "Stop")]
+
+namespace StoreManagement.API.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<IStoreContext>().To<StoreContext>().WithConstructorArgument("nameOrConnectionString","Stores");
+            kernel.Bind<IContentRepository>().To<ContentRepository>();
+            kernel.Bind<IStoreRepository>().To<StoreRepository>();
+            kernel.Bind<ISettingRepository>().To<SettingRepository>();
+            kernel.Bind<IFileManagerRepository>().To<FileManagerRepository>();
+            kernel.Bind<INavigationRepository>().To<NavigationRepository>();
+            kernel.Bind<ICategoryRepository>().To<CategoryRepository>();
+            kernel.Bind<IPageDesignRepository>().To<PageDesignRepository>();
+            kernel.Bind<IContentFileRepository>().To<ContentFileRepository>();
+            kernel.Bind<IStoreUserRepository>().To<StoreUserRepository>();
+            kernel.Bind<ICompanyRepository>().To<CompanyRepository>();
+
+        }        
+    }
+}
---
+++ StoreManagement/StoreManagement.API/Controllers/BaseApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ninject;
+using StoreManagement.Service.Repositories.Interfaces;
+
+namespace StoreManagement.API.Controllers
+{
+    public abstract class BaseApiController : ApiController
+    {
+
+        [Inject]
+        public IFileManagerRepository FileManagerRepository { get; set; }
+
+        [Inject]
+        public IContentFileRepository ContentFileRepository { set; get; }
+
+        [Inject]
+        public IContentRepository ContentRepository { set; get; }
+
+        [Inject]
+        public ICategoryRepository CategoryRepository { set; get; }
+
+        [Inject]
+        public IStoreRepository StoreRepository { set; get; }
+
+        [Inject]
+        public INavigationRepository NavigationRepository { set; get; }
+
+        [Inject]
+        public IPageDesignRepository PageDesignRepository { set; get; }
+
+        [Inject]
+        public IStoreUserRepository StoreUserRepository { set; get; }
+
+
+    }
+}

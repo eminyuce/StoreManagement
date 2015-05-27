5378c9bb6df8d6694633f02eb7e6b071e236c198
eminyuce <eminyuce@gmail.com>
2015-05-27 12:20:47 -0400

    Adding one missing file.

---
+++ StoreManagement/StoreManagement.Service/Interfaces/IStoreService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using StoreManagement.Data.Entities;
+
+namespace StoreManagement.Service.Interfaces
+{
+    public interface IStoreService
+    {
+        Store GetStoreByDomain(string domainName);
+        Store GetStore(HttpRequestBase request);
+        Store GetSingle(int id);
+
+
+    }
+}

3154a5d941f33e81f575e36e8dccf6d6b28fdaa2
eminyuce <eminyuce@gmail.com>
2015-06-09 10:15:58 -0400

    Updating jquery scripts

--- StoreManagement/StoreManagement/Controllers/BlogsController.cs
+++ StoreManagement/StoreManagement/Controllers/BlogsController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MvcPaging;
 using StoreManagement.Data.Entities;
+using StoreManagement.Data.GeneralHelper;
 using StoreManagement.Data.RequestModel;
 
 namespace StoreManagement.Controllers
@@ -21,6 +22,22 @@ namespace StoreManagement.Controllers
             var m = ContentService.GetContentsCategoryId(Store.Id, categoryId, "blog", true, page, 24);
             newsContents.Contents = new PagedList<Content>(m.items, m.page - 1, m.pageSize, m.totalItemCount);
             return View(newsContents);
+        }
+        public ActionResult Blog(String id)
+        {
+            var returnModel = new ContentDetailViewModel();
+            int blogId = id.Split("-".ToCharArray()).Last().ToInt();
+            returnModel.Content = ContentService.GetContentsContentId(blogId);
+            returnModel.Store = Store;
+            returnModel.Category = CategoryService.GetSingle(returnModel.Content.CategoryId);
+            returnModel.Categories = CategoryService.GetCategoriesByStoreId(Store.Id, "blog");
+            var c = new List<Content>();
+            c.Add(returnModel.Content);
+            returnModel.RelatedContents = ContentService.GetContentByTypeAndCategoryId(Store.Id, "blog",
+                returnModel.Content.CategoryId).Except(c).Take(5).ToList();
+
+
+            return View(returnModel);
         }
     }
 }

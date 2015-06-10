02092d1bb9c8efad991fe6cc2be3d56adbe440c5
eminyuce <eminyuce@gmail.com>
2015-06-10 17:13:29 -0400

    Admin News Page Development

---
+++ StoreManagement/StoreManagement.Admin/Controllers/NewsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreManagement.Data.Entities;
+using StoreManagement.Data.RequestModel;
+using StoreManagement.Service.DbContext;
+using StoreManagement.Service.Repositories.Interfaces;
+
+namespace StoreManagement.Admin.Controllers
+{
+    [Authorize]
+    public class NewsController : BaseController
+    {
+        private const String ContentType = "news";
+        public NewsController(IStoreContext dbContext, ISettingRepository settingRepository)
+            : base(dbContext, settingRepository)
+        {
+             
+        }
+
+        public ActionResult Index(int storeId=0, String search="", int categoryId=0)
+        {
+            List<Content> resultList = new List<Content>();
+            storeId = GetStoreId(storeId);
+            if (storeId == 0)
+            {
+                resultList = ContentRepository.GetContentByType(ContentType);
+            }
+            else
+            {
+                resultList = ContentRepository.GetContentByType(storeId, ContentType);
+            }
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                resultList =
+                    resultList.Where(r => r.Name.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            }
+
+            if (categoryId > 0)
+            {
+                resultList = resultList.Where(r => r.CategoryId == categoryId ).ToList();
+            }
+            var contentsAdminViewModel = new ContentsAdminViewModel();
+            contentsAdminViewModel.Contents = resultList;
+            contentsAdminViewModel.Categories = CategoryRepository.GetCategoriesByStoreIdFromCache(storeId, ContentType);
+            return View(contentsAdminViewModel);
+        }
+
+        //
+        // GET: /Content/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Content content = ContentRepository.GetSingle(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        //
+        // GET: /Content/Create
+
+        public ActionResult SaveOrEdit(int id=0)
+        {
+            var content = new Content();
+            if (id == 0)
+            {
+                content.Type = ContentType;
+            }
+            else
+            {
+                content = ContentRepository.GetSingle(id);
+            }
+            return View(content);
+        }
+
+        //
+        // POST: /Content/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveOrEdit(Content content, int [] selectedFileId = null)
+        {
+            if (ModelState.IsValid)
+            {
+                content.Description = GetCleanHtml(content.Description);
+                if (content.Id == 0)
+                {
+                    ContentRepository.Add(content);
+                }
+                else
+                {
+                    ContentRepository.Edit(content);
+                }
+                content.CreatedDate = DateTime.Now;
+                ContentRepository.Save();
+                if (selectedFileId != null)
+                {
+                    int contentId = content.Id;
+                    ContentFileRepository.SaveContentFiles(selectedFileId, contentId);
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(content);
+        }
+
+      
+
+        //
+        // GET: /Content/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Content content = ContentRepository.GetSingle(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        //
+        // POST: /Content/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Content content = ContentRepository.GetSingle(id);
+            ContentRepository.Delete(content);
+            ContentRepository.Save();
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}

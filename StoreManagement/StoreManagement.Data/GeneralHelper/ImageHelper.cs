24a721858219bf90db3688730108df0f9cb7f591
eminyuce@gmail.com <eminyuce@gmail.com>
2015-07-07 06:24:33 -0400

    ImageHelper adding thumbnail image method

--- StoreManagement/StoreManagement.Data/GeneralHelper/ImageHelper.cs
+++ StoreManagement/StoreManagement.Data/GeneralHelper/ImageHelper.cs
@@ -41,7 +41,99 @@ namespace StoreManagement.Data.GeneralHelper
                 return ImageOrientation.Unknown;
             }
         }
+        public static byte[] MakeThumbnail(byte[] myImage, int thumbWidth, int thumbHeight)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            using (Image thumbnail = Image.FromStream(new MemoryStream(myImage)).GetThumbnailImage(thumbWidth, thumbHeight, null, new IntPtr()))
+            {
+                thumbnail.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+        // (RESIZE an image in a byte[] variable.)  
+        public static byte[] CreateThumbnail(byte[] PassedImage, int LargestSide)
+        {
+            byte[] ReturnedThumbnail;
 
+            using (MemoryStream StartMemoryStream = new MemoryStream(),
+                                NewMemoryStream = new MemoryStream())
+            {
+                // write the string to the stream  
+                StartMemoryStream.Write(PassedImage, 0, PassedImage.Length);
+
+                // create the start Bitmap from the MemoryStream that contains the image  
+                Bitmap startBitmap = new Bitmap(StartMemoryStream);
+
+                // set thumbnail height and width proportional to the original image.  
+                //int newHeight;
+                //int newWidth;
+                //double HW_ratio;
+                //if (startBitmap.Height > startBitmap.Width)
+                //{
+                //    newHeight = LargestSide;
+                //    HW_ratio = (double)((double)LargestSide / (double)startBitmap.Height);
+                //    newWidth = (int)(HW_ratio * (double)startBitmap.Width);
+                //}
+                //else
+                //{
+                //    newWidth = LargestSide;
+                //    HW_ratio = (double)((double)LargestSide / (double)startBitmap.Width);
+                //    newHeight = (int)(HW_ratio * (double)startBitmap.Height);
+                //}
+                int newHeight;
+                int newWidth;
+                double HW_ratio;
+                if (startBitmap.Height > LargestSide)
+                // If the original image isn't larger on any side than LargestSide
+                //  then don't increase the size of the image.gestSide || startBitmap.Width > LargestSide)
+                {
+                    if (startBitmap.Height > startBitmap.Width)
+                    {
+                        newHeight = LargestSide;
+                        HW_ratio = (double)((double)LargestSide / (double)startBitmap.Height);
+                        newWidth = (int)(HW_ratio * (double)startBitmap.Width);
+                    }
+                    else
+                    {
+                        newWidth = LargestSide;
+                        HW_ratio = (double)((double)LargestSide / (double)startBitmap.Width);
+                        newHeight = (int)(HW_ratio * (double)startBitmap.Height);
+                    }
+                }
+                else
+                {
+                    newHeight = startBitmap.Height;
+                    newWidth = startBitmap.Width;
+                }
+                // create a new Bitmap with dimensions for the thumbnail.  
+                Bitmap newBitmap = new Bitmap(newWidth, newHeight);
+
+                // Copy the image from the START Bitmap into the NEW Bitmap.  
+                // This will create a thumnail size of the same image.  
+                newBitmap = ResizeImage(startBitmap, newWidth, newHeight);
+
+                // Save this image to the specified stream in the specified format.  
+                newBitmap.Save(NewMemoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                // Fill the byte[] for the thumbnail from the new MemoryStream.  
+                ReturnedThumbnail = NewMemoryStream.ToArray();
+            }
+
+            // return the resized image as a string of bytes.  
+            return ReturnedThumbnail;
+        }
+
+        // Resize a Bitmap  
+        private static Bitmap ResizeImage(Bitmap image, int width, int height)
+        {
+            Bitmap resizedImage = new Bitmap(width, height);
+            using (Graphics gfx = Graphics.FromImage(resizedImage))
+            {
+                gfx.DrawImage(image, new Rectangle(0, 0, width, height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+            }
+            return resizedImage;
+        } 
         private bool IsImage(string ext)
         {
             return ext == ".gif" || ext == ".jpg" || ext == ".png";

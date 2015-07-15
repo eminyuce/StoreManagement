ab3aa81168c52990e272cede7de224df29779a48
eminyuce <eminyuce@gmail.com>
2015-07-15 17:17:04 -0400

    Page Design development bug fixed.

--- StoreManagement/StoreManagement.Admin/Controllers/PageDesignsController.cs
+++ StoreManagement/StoreManagement.Admin/Controllers/PageDesignsController.cs
@@ -136,6 +136,8 @@ namespace StoreManagement.Admin.Controllers
                 //Log the error (uncomment dex variable name and add a line here to write a log.
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
             }
+
+            return View(pagedesign);
         }
 
     }

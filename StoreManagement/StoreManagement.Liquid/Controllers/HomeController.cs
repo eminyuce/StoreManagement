6e5f703d9fdd956f982b56f8cbb086ad580fdcc4
eminyuce <eminyuce@gmail.com>
2015-07-20 16:29:31 -0400

    DotLiquid ViewEngine is began to get used

---
+++ StoreManagement/StoreManagement.Liquid/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreManagement.Liquid.Controllers
+{
+    public class HomeController : BaseController
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
---
+++ StoreManagement/StoreManagement.Liquid/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreManagement.Liquid.Controllers
+{
+    public class ProductsController : BaseController
+    {
+        //
+        // GET: /Products/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Product2()
+        {
+            return View();
+        }
+        
+	}
+}

acf5a5dba9d662b91e17abfcb13d2ec5e9ddba24
eminyuce <eminyuce@gmail.com>
2015-12-30 15:02:53 -0500

    Liquid Template Testing

---
+++ StoreManagement/StoreManagement/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using StoreManagement.Data.Constants;
+using StoreManagement.Data.LiquidEntities;
+
+namespace StoreManagement.Controllers
+{
+    public class TestController : BaseController
+    {
+        //
+        // GET: /Test/
+        public async Task<ActionResult> Index()
+        {
+
+            var productsTask = ProductService.GetProductsAsync(this.StoreId, 10, true);
+            var categoriesTask = ProductCategoryService.GetProductCategoriesByStoreIdAsync(StoreId, StoreConstants.ProductType, true);
+            await Task.WhenAll(productsTask, categoriesTask);
+            var productsObk = productsTask.Result;
+            var categories = categoriesTask.Result;
+            var products = new List<ProductLiquid>();
+            foreach (var item in productsObk)
+            {
+                var category = categories.FirstOrDefault(r => r.Id == item.ProductCategoryId);
+                if (category != null)
+                {
+                    var blog = new ProductLiquid(item, category, 10, 10);
+                    products.Add(blog);
+                }
+            }
+
+            return View(products);
+        }
+	}
+}

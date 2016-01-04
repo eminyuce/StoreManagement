ca36309f05c99164e5987cd1387dfa22161ebe31
eminyuce <eminyuce@gmail.com>
2016-01-03 20:35:24 -0500

    Ajax dynamic content development

--- StoreManagement/StoreManagement/Controllers/AjaxContentsController.cs
+++ StoreManagement/StoreManagement/Controllers/AjaxContentsController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using NLog;
 using StoreManagement.Data.Attributes;
+using StoreManagement.Data.Constants;
+using StoreManagement.Data.Entities;
 using StoreManagement.Data.GeneralHelper;
 using StoreManagement.Data.RequestModel;
 
@@ -32,6 +35,191 @@ namespace StoreManagement.Controllers
             var html = this.RenderPartialToString(partialViewName, new ViewDataDictionary(returnModel));
             return Json(html, JsonRequestBehavior.AllowGet);
         }
-     
+
+
+        public async Task<JsonResult> GetRelatedContents(int categoryId, String contentType, int excludedContentId = 0, String designName = "", int take = 0, int imageWidth = 0, int imageHeight = 0)
+        {
+
+            if (String.IsNullOrEmpty(designName))
+            {
+                return Json("No Desing Name is defined.", JsonRequestBehavior.AllowGet);
+            }
+
+            String returnHtml = "";
+
+            try
+            {
+                returnHtml = await GetRelatedContentsHtml(categoryId, contentType, excludedContentId, designName, take, imageWidth, imageHeight);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "GetRelatedContents:" + ex.StackTrace, StoreId, contentType, categoryId, take, excludedContentId);
+
+            }
+
+
+            return Json(returnHtml, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<String> GetRelatedContentsHtml(int categoryId, string contentType, int excludedContentId, string designName, int take,
+            int imageWidth, int imageHeight)
+        {
+            string returnHtml;
+            var categoryTask = CategoryService.GetCategoryAsync(categoryId);
+
+
+            if (take == 0 && contentType.Equals(StoreConstants.NewsType))
+            {
+                take = GetSettingValueInt("RelatedNews_ItemsNumber", 5);
+            }
+            else if (take == 0 && contentType.Equals(StoreConstants.BlogsType))
+            {
+                take = GetSettingValueInt("RelatedBlogs_ItemsNumber", 5);
+            }
+
+            var relatedContentsTask = ContentService.GetContentByTypeAndCategoryIdAsync(StoreId, contentType, categoryId, take,
+                excludedContentId);
+            var pageDesignTask = PageDesignService.GetPageDesignByName(StoreId, designName);
+
+            ContentHelper.StoreSettings = GetStoreSettings();
+
+
+            if (contentType.Equals(StoreConstants.NewsType))
+            {
+                ContentHelper.ImageWidth = imageWidth == 0
+                    ? GetSettingValueInt("RelatedNewsPartial_ImageWidth", 50)
+                    : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0
+                    ? GetSettingValueInt("RelatedNewsPartial_ImageHeight", 50)
+                    : imageHeight;
+            }
+            else if (contentType.Equals(StoreConstants.BlogsType))
+            {
+                ContentHelper.ImageWidth = imageWidth == 0
+                    ? GetSettingValueInt("RelatedBlogsPartial_ImageWidth", 50)
+                    : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0
+                    ? GetSettingValueInt("RelatedBlogsPartial_ImageHeight", 50)
+                    : imageHeight;
+            }
+            else
+            {
+                ContentHelper.ImageWidth = 0;
+                ContentHelper.ImageHeight = 0;
+                Logger.Trace("No ContentType is defined like that " + contentType);
+            }
+
+            await Task.WhenAll(pageDesignTask, relatedContentsTask, categoryTask);
+            var contents = relatedContentsTask.Result;
+            var pageDesign = pageDesignTask.Result;
+            var category = categoryTask.Result;
+
+            var pageOutput = ContentHelper.GetRelatedContentsPartial(category, contents, pageDesign, contentType);
+            returnHtml = pageOutput.PageOutputText;
+
+            return returnHtml;
+        }
+
+
+        public async Task<JsonResult> GetContentsByContentType(int page = 1, String designName = "ContentsByContentTypePartial", int categoryId = 0,
+            int pageSize = 0, int imageWidth = 0, int imageHeight = 0, String type = StoreConstants.BlogsType, String contentType = "popular", int excludedContentId = 0)
+        {
+
+            if (String.IsNullOrEmpty(designName))
+            {
+                return Json("No Desing Name is defined.", JsonRequestBehavior.AllowGet);
+            }
+            String returnHtml = "";
+
+            try
+            {
+
+                returnHtml = await GetContentsByContentTypeHtml(page, pageSize, designName, categoryId, imageWidth, imageHeight, type, contentType);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "ContentsByContentTypePartial:" + ex.StackTrace, StoreId, categoryId, type, page, pageSize, contentType);
+
+            }
+
+
+            return Json(returnHtml, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<String> GetContentsByContentTypeHtml(int page, int pageSize, string designName, int categoryId, int imageWidth, int imageHeight,
+            string type, string contentType)
+        {
+
+            string returnHtml;
+            var catId = categoryId == 0 ? (int?)null : categoryId;
+            if (contentType.Equals("random"))
+            {
+                pageSize = pageSize == 0
+                    ? GetSettingValueInt("RandomContents_PageSize", StoreConstants.DefaultPageSize)
+                    : pageSize;
+                ContentHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("PopularContents_ImageWidth", 99) : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0
+                    ? GetSettingValueInt("PopularContents_ImageHeight", 99)
+                    : imageHeight;
+            }
+            else if (contentType.Equals("normal"))
+            {
+                pageSize = pageSize == 0
+                    ? GetSettingValueInt("NormalContents_PageSize", StoreConstants.DefaultPageSize)
+                    : pageSize;
+                ContentHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("PopularContents_ImageWidth", 99) : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0
+                    ? GetSettingValueInt("PopularContents_ImageHeight", 99)
+                    : imageHeight;
+            }
+            else if (contentType.Equals("popular"))
+            {
+                pageSize = pageSize == 0
+                    ? GetSettingValueInt("PopularContents_PageSize", StoreConstants.DefaultPageSize)
+                    : pageSize;
+                ContentHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("PopularContents_ImageWidth", 99) : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0
+                    ? GetSettingValueInt("PopularContents_ImageHeight", 99)
+                    : imageHeight;
+            }
+            else if (contentType.Equals("recent"))
+            {
+                pageSize = pageSize == 0
+                    ? GetSettingValueInt("RecentContents_PageSize", StoreConstants.DefaultPageSize)
+                    : pageSize;
+                ContentHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("RecentContents_ImageWidth", 99) : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0
+                    ? GetSettingValueInt("RecentContents_ImageHeight", 99)
+                    : imageHeight;
+            }
+            else if (contentType.Equals("main"))
+            {
+                pageSize = pageSize == 0
+                    ? GetSettingValueInt("MainContents_PageSize", StoreConstants.DefaultPageSize)
+                    : pageSize;
+                ContentHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("MainContents_ImageWidth", 99) : imageWidth;
+                ContentHelper.ImageHeight = imageHeight == 0 ? GetSettingValueInt("MainContents_ImageHeight", 99) : imageHeight;
+            }
+            Task<List<Content>> contentsTask = ContentService.GetContentsByContentKeywordAsync(StoreId, catId, type, page,
+                pageSize, true, contentType);
+
+
+            var pageDesignTask = PageDesignService.GetPageDesignByName(StoreId, designName);
+            var categoriesTask = CategoryService.GetCategoriesByStoreIdAsync(StoreId, type, true);
+
+            await Task.WhenAll(pageDesignTask, contentsTask, categoriesTask);
+            var contents = contentsTask.Result;
+            var pageDesign = pageDesignTask.Result;
+            var categories = categoriesTask.Result;
+
+            ContentHelper.StoreSettings = GetStoreSettings();
+
+
+            var pageOuput = ContentHelper.GetContentsByContentType(contents, categories, pageDesign, type);
+            returnHtml = pageOuput.PageOutputText;
+
+            return returnHtml;
+        }
+
 	}
 }
--- StoreManagement/StoreManagement/Controllers/AjaxProductsController.cs
+++ StoreManagement/StoreManagement/Controllers/AjaxProductsController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using DotLiquid.FileSystems;
 using NLog;
 using StoreManagement.Data.Attributes;
 using StoreManagement.Data.Constants;
+using StoreManagement.Data.Entities;
 using StoreManagement.Data.GeneralHelper;
 using StoreManagement.Data.RequestModel;
 
@@ -46,5 +48,284 @@ namespace StoreManagement.Controllers
             return Json(html, JsonRequestBehavior.AllowGet);
         }
 
+
+        public async Task<JsonResult> GetProductCategories(String designName = "ProductCategoriesPartial", int imageWidth = 0, int imageHeight = 0)
+        {
+
+            if (String.IsNullOrEmpty(designName))
+            {
+                return Json("No Desing Name is defined.", JsonRequestBehavior.AllowGet);
+            }
+            String returnHtml = "";
+
+            try
+            {
+                returnHtml = await GetProductCategoriesHtml(designName, imageWidth, imageHeight);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "GetProductCategories:" + ex.StackTrace, designName);
+
+            }
+
+
+
+            return Json(returnHtml, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<String> GetProductCategoriesHtml(string designName, int imageWidth, int imageHeight)
+        {
+            string returnHtml;
+            var categoriesTask = ProductCategoryService.GetProductCategoriesByStoreIdAsync(StoreId, StoreConstants.ProductType,
+                                                                                           true);
+            var pageDesignTask = PageDesignService.GetPageDesignByName(StoreId, designName);
+
+            ProductCategoryHelper.StoreSettings = GetStoreSettings();
+            ProductCategoryHelper.ImageWidth = imageWidth == 0
+                                                   ? GetSettingValueInt("ProductCategoriesPartial_ImageWidth", 50)
+                                                   : imageWidth;
+            ProductCategoryHelper.ImageHeight = imageHeight == 0
+                                                    ? GetSettingValueInt("ProductCategoriesPartial_ImageHeight", 50)
+                                                    : imageHeight;
+
+            await Task.WhenAll(categoriesTask, pageDesignTask);
+            var categories = categoriesTask.Result;
+            var pageDesign = pageDesignTask.Result;
+
+            var pageOuput = ProductCategoryHelper.GetProductCategoriesPartial(categories, pageDesign);
+            returnHtml = pageOuput.PageOutputText;
+
+            return returnHtml;
+        }
+
+
+        public async Task<JsonResult> GetBrands(String designName = "BrandsPartial", int take = 0,
+            int imageWidth = 0,
+            int imageHeight = 0)
+        {
+
+            if (String.IsNullOrEmpty(designName))
+            {
+                return Json("No Desing Name is defined.", JsonRequestBehavior.AllowGet);
+            }
+
+            String returnHtml = "";
+
+            try
+            {
+                returnHtml = await GetBrandsHtml(designName, take, imageWidth, imageHeight);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "GetBrands:" + ex.StackTrace, designName);
+
+            }
+
+
+            return Json(returnHtml, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<String> GetBrandsHtml(string designName, int take, int imageWidth, int imageHeight)
+        {
+            string returnHtml;
+            take = take == 0 ? GetSettingValueInt("BrandsPartial_ItemsNumber", 50) : take;
+            var pageDesignTask = PageDesignService.GetPageDesignByName(StoreId, designName);
+            var brandsTask = BrandService.GetBrandsAsync(StoreId, take, true);
+
+            BrandHelper.StoreSettings = GetStoreSettings();
+            BrandHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("BrandsPartial_ImageWidth", 50) : imageWidth;
+            BrandHelper.ImageHeight = imageHeight == 0 ? GetSettingValueInt("BrandsPartial_ImageHeight", 50) : imageHeight;
+
+            await Task.WhenAll(pageDesignTask, brandsTask);
+            var pageDesign = pageDesignTask.Result;
+            var brands = brandsTask.Result;
+            if (pageDesign == null)
+            {
+                throw new Exception("PageDesing is null");
+            }
+
+
+            var pageOuput = BrandHelper.GetBrandsPartial(brands, pageDesign);
+            returnHtml = pageOuput.PageOutputText;
+
+            return returnHtml;
+        }
+
+        public async Task<JsonResult> GetProductLabels(int id, String designName = "ProductLabelsPartial", int imageWidth = 0, int imageHeight = 0)
+        {
+
+            if (String.IsNullOrEmpty(designName))
+            {
+                return Json("No Desing Name is defined.", JsonRequestBehavior.AllowGet);
+            }
+            String returnHtml = "";
+            String key = String.Format("GetProductLabels-{0}-{1}-{2}-{3}", id, designName, imageWidth, imageHeight);
+            try
+            {
+                returnHtml = await GetProductLabelsHtml(id, designName, imageWidth, imageHeight);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "GetProductLabels:" + ex.StackTrace, designName);
+            }
+
+
+            return Json(returnHtml, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<String> GetProductLabelsHtml(int id, string designName, int imageWidth, int imageHeight)
+        {
+            string returnHtml;
+            var pageDesignTask = PageDesignService.GetPageDesignByName(StoreId, designName);
+            var labelsTask = LabelService.GetLabelsByItemTypeId(StoreId, id, StoreConstants.ProductType);
+
+            LabelHelper.StoreSettings = GetStoreSettings();
+            LabelHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("ProductLabels_ImageWidth", 50) : imageWidth;
+            LabelHelper.ImageHeight = imageHeight == 0 ? GetSettingValueInt("ProductLabels_ImageHeight", 50) : imageHeight;
+
+
+            await Task.WhenAll(pageDesignTask, labelsTask);
+            var labels = labelsTask.Result;
+            var pageDesign = pageDesignTask.Result;
+
+            if (pageDesign == null)
+            {
+                throw new Exception("PageDesing is null");
+            }
+
+
+            var pageOuput = LabelHelper.GetProductLabels(labels, pageDesign);
+            returnHtml = pageOuput.PageOutputText;
+
+            return returnHtml;
+        }
+
+        public async Task<JsonResult> GetProductsByProductType(int page = 1, String designName = "", int categoryId = 0, int brandId = 0, int retailerId = 0,
+            int pageSize = 0, int imageWidth = 0, int imageHeight = 0, String productType = "popular", int excludedProductId = 0)
+        {
+
+
+            if (String.IsNullOrEmpty(designName))
+            {
+                return Json("No Desing Name is defined.", JsonRequestBehavior.AllowGet);
+            }
+            String returnHtml = "";
+            String key = String.Format("GetProductsByProductType-{0}-{1}-{2}-{3}-{4}-{5}-{6}-{7}-{8}-{9}",
+                StoreId, page, designName, categoryId, brandId, pageSize, imageHeight, imageWidth, productType, excludedProductId);
+            try
+            {
+
+                returnHtml = await GetProductsByProductTypeHtml(page, designName, categoryId, brandId, retailerId, pageSize, imageWidth, imageHeight, productType, excludedProductId);
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "GetProductsByProductType:" + ex.StackTrace, page, designName, categoryId, brandId, pageSize, imageWidth, imageHeight, productType, excludedProductId);
+
+            }
+
+
+            return Json(returnHtml, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<String> GetProductsByProductTypeHtml(int page, string designName, int categoryId, int brandId, int retailerId, int pageSize,
+                                                        int imageWidth, int imageHeight, string productType, int excludedProductId)
+        {
+
+            string returnHtml = "";
+
+
+            Task<List<Product>> productsTask = null;
+            var catId = categoryId == 0 ? (int?)null : categoryId;
+            var retId = retailerId == 0 ? (int?)null : retailerId;
+            var bId = brandId == 0 ? (int?)null : brandId;
+            var eProductId = excludedProductId == 0 ? (int?)null : excludedProductId;
+            Logger.Trace("StoreId " + StoreId +
+                         " designName:" +
+                         designName +
+                         " categoryId:" +
+                         categoryId + " brandId:" +
+                         brandId + " pageSize:" + pageSize + " page:" +
+                         page + " imageWidth:" + imageWidth + " imageHeight:" + imageHeight + " productType:" + productType);
+
+            if (productType.Equals("random"))
+            {
+                pageSize = pageSize == 0
+                               ? GetSettingValueInt("RandomProducts_PageSize", StoreConstants.DefaultPageSize)
+                               : pageSize;
+                ProductHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("RandomProducts_ImageWidth", 99) : imageWidth;
+                ProductHelper.ImageHeight = imageHeight == 0
+                                                ? GetSettingValueInt("RandomProducts_ImageHeight", 99)
+                                                : imageHeight;
+            }
+            else if (productType.Equals("normal"))
+            {
+                pageSize = pageSize == 0
+                               ? GetSettingValueInt("NormalProducts_PageSize", StoreConstants.DefaultPageSize)
+                               : pageSize;
+                ProductHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("NormalProducts_ImageWidth", 99) : imageWidth;
+                ProductHelper.ImageHeight = imageHeight == 0
+                                                ? GetSettingValueInt("NormalProducts_ImageHeight", 99)
+                                                : imageHeight;
+            }
+            else if (productType.Equals("popular"))
+            {
+                pageSize = pageSize == 0
+                               ? GetSettingValueInt("PopularProducts_PageSize", StoreConstants.DefaultPageSize)
+                               : pageSize;
+                ProductHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("PopularProducts_ImageWidth", 99) : imageWidth;
+                ProductHelper.ImageHeight = imageHeight == 0
+                                                ? GetSettingValueInt("PopularProducts_ImageHeight", 99)
+                                                : imageHeight;
+            }
+            else if (productType.Equals("recent"))
+            {
+                pageSize = pageSize == 0
+                               ? GetSettingValueInt("RecentProducts_PageSize", StoreConstants.DefaultPageSize)
+                               : pageSize;
+                ProductHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("RecentProducts_ImageWidth", 99) : imageWidth;
+                ProductHelper.ImageHeight = imageHeight == 0
+                                                ? GetSettingValueInt("RecentProducts_ImageHeight", 99)
+                                                : imageHeight;
+            }
+            else if (productType.Equals("main"))
+            {
+                pageSize = pageSize == 0
+                               ? GetSettingValueInt("MainProducts_PageSize", StoreConstants.DefaultPageSize)
+                               : pageSize;
+                ProductHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("MainProducts_ImageWidth", 99) : imageWidth;
+                ProductHelper.ImageHeight = imageHeight == 0 ? GetSettingValueInt("MainProducts_ImageHeight", 99) : imageHeight;
+            }
+            else if (productType.Equals("discount"))
+            {
+                pageSize = pageSize == 0
+                               ? GetSettingValueInt("DiscountProducts_PageSize", StoreConstants.DefaultPageSize)
+                               : pageSize;
+                ProductHelper.ImageWidth = imageWidth == 0 ? GetSettingValueInt("DiscountProducts_ImageWidth", 99) : imageWidth;
+                ProductHelper.ImageHeight = imageHeight == 0
+                                                ? GetSettingValueInt("DiscountProducts_ImageHeight", 99)
+                                                : imageHeight;
+            }
+
+            productsTask = ProductService.GetProductsByProductType(StoreId, catId, bId, retId, StoreConstants.ProductType, page,
+                                                                 pageSize, true, productType, eProductId);
+            var pageDesignTask = PageDesignService.GetPageDesignByName(StoreId, designName);
+            var productCategoriesTask = ProductCategoryService.GetProductCategoriesByStoreIdAsync(StoreId,
+                                                                                                  StoreConstants
+                                                                                                      .ProductType, true);
+
+            await Task.WhenAll(pageDesignTask, productsTask, productCategoriesTask);
+            var products = productsTask.Result;
+            var pageDesign = pageDesignTask.Result;
+            var productCategories = productCategoriesTask.Result;
+
+            ProductHelper.StoreSettings = GetStoreSettings();
+
+
+            var pageOuput = ProductHelper.GetPopularProducts(products, productCategories, pageDesign);
+            returnHtml = pageOuput.PageOutputText;
+
+            return returnHtml;
+        }
 	}
 }

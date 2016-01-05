8e6bbc0070d3c8ce6c93cb8dcfb271429e230839
eminyuce <eminyuce@gmail.com>
2016-01-05 16:21:18 -0500

    Liquid pages

--- StoreManagement/StoreManagement/App_Start/RouteConfig.cs
+++ StoreManagement/StoreManagement/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace StoreManagement
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.LowercaseUrls = true;
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(

2187f1e8366d704496d6b1140dd973419fceb540
eminyuce@gmail.com <eminyuce@gmail.com>
2016-05-01 13:41:33 -0400

    DataHelper methods

--- StoreManagement/StoreManagement.Data/GeneralHelper/DataTableHelper.cs
+++ StoreManagement/StoreManagement.Data/GeneralHelper/DataTableHelper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -57,6 +60,134 @@ namespace StoreManagement.Data.GeneralHelper
 
             // Print bottom line
             Console.WriteLine(new string('-', 75));
+        }
+
+
+        public static void AddColumn<T>(DataTable dataTable, string columnName, bool nullable)
+        {
+            var col = dataTable.Columns.Add(columnName, typeof(T));
+            col.AllowDBNull = nullable;
+        }
+
+        public static void AddColumn<T>(DataTable dataTable, string columnName)
+        {
+              AddColumn<T>(dataTable,columnName, true);
+        }
+
+        public static void AddRows<T>(DataTable dataTable, List<T> items, Func<T, object[]> mapper)
+        {
+            items.ForEach(x => dataTable.Rows.Add(mapper(x)));
+        }
+        /// <summary>
+        /// Converts to.
+        /// </summary>
+        /// <returns>The to.</returns>
+        /// <param name="list">List.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static DataTable ConvertTo<T>(IList<T> paramList)
+        {
+            DataTable table = CreateTable<T>();
+            Type entityType = typeof(T);
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entityType);
+
+            foreach (T item in paramList)
+            {
+                DataRow row = table.NewRow();
+
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    row[prop.Name] = prop.GetValue(item);
+                }
+
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Creates the table.
+        /// </summary>
+        /// <returns>The table.</returns>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static DataTable CreateTable<T>()
+        {
+            Type entityType = typeof(T);
+            DataTable table = new DataTable(entityType.Name);
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entityType);
+
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            return table;
+        }
+        public static string[] GetFieldAsArray(DataTable dt, string fieldName)
+        {
+            List<string> data = new List<string>();
+
+            int index = dt.Columns.IndexOf(fieldName);
+
+            if (index < 0)
+            {
+                return new string[] { };
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                data.Add(row[index].ToString());
+            }
+
+            return data.ToArray();
+        }
+        /// <summary>
+        /// DataTable extension for deleteing rows
+        /// </summary>
+        /// <param name="table">DataTable</param>
+        /// <param name="filter">Filter expression</param>
+        /// <returns>Altered DataTable</returns>
+        public static DataTable Delete(DataTable table, string filter)
+        {
+            Delete(table.Select(filter));
+            return table;
+        }
+
+        /// <summary>
+        /// Delete function for DataTable
+        /// </summary>
+        /// <param name="rows">DataRows in DataTable</param>
+        public static void Delete(IEnumerable<DataRow> rows)
+        {
+            foreach (var row in rows)
+                row.Delete();
+        }
+        public static T[] ToArray<R, T>(DataTable table, Func<R, T> converter) where R : DataRow
+        {
+            if (table.Rows.Count == 0)
+            {
+                return new T[] { };
+            }
+            //Verify [DataContract] or [Serializable] on T
+            Debug.Assert(IsDataContract(typeof(T)) || typeof(T).IsSerializable);
+
+            //Verify table contains correct rows 
+            Debug.Assert(MatchingTableRow<R>(table));
+
+            return table.Rows.Cast<R>().Select(converter).ToArray();
+        }
+        static bool IsDataContract(Type type)
+        {
+            object[] attributes = type.GetCustomAttributes(typeof(DataContractAttribute), false);
+            return attributes.Length == 1;
+        }
+        static bool MatchingTableRow<R>(DataTable table)
+        {
+            if (table.Rows.Count == 0)
+            {
+                return true;
+            }
+            return table.Rows[0] is R;
         }
     }
 }
